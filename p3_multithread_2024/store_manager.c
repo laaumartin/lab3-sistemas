3db84142f4888b746a375366ddf5eb519dcafbf7
luciacaldado <165171765+luciacaldado@users.noreply.github.com>
2024-05-06 20:35:00 +0000

    act

--- p3_multithread_2024/store_manager.c
+++ p3_multithread_2024/store_manager.c
@@ -74,76 +74,99 @@ void *consumers(void *args){
 
 int main (int argc, const char * argv[])
 {
-  int profits = 0;
-  int product_stock [5] = {0};
+	//Initialize mutex and cond_vars
+	pthread_mutex_init(&mutex, NULL);
+	pthread_cond_init(&non_full, NULL);
+	pthread_cond_init(&non_empty, NULL);
 
-  	// Reading Input arguments
-	if (argc !=5){ // check correct number of inputs
-		perror("./store manager <file name><num producers><num consumers><buff size>");
-		return -1;
-	}
+  	int profits = 0;
+	int product_stock [5] = {0};
 
-	const char *fileName = argv[1];
-	int prods = atoi(argv[2]);
-	int cons = atoi(argv[3]);
-	int bsize = atoi(argv[4]);
+		// Reading Input arguments
+		if (argc !=5){ // check correct number of inputs
+			perror("./store manager <file name><num producers><num consumers><buff size>");
+			return -1;
+		}
+
+		const char *fileName = argv[1];
+		int prods = atoi(argv[2]);
+		int cons = atoi(argv[3]);
+		int bsize = atoi(argv[4]);
+			
+		if(prods<=0){
+			perror("Invalid number of producers");
+			return -1;
+		}
 		
-	if(prods<=0){
-		perror("Invalid number of producers");
-		return -1;
-	}
-	
-	if(cons<=0){
-		perror("Invalid number of consumers");
-		return -1;
-	}
-	if(bsize<=0){
-		perror("Invalid size of buffer");
-		return -1;
-	}
-	
-	// ***************** Loading data from input file into memory ****************
-	// Reading the input file
-	FILE *fidin = NULL;
-	
-	fidin = fopen(fileName,"r"); // Open the file
-	if (!fidin){
-		perror("Error opening the file");
-		return 1;
-	}
-	if(fscanf(fidin,"%d", &num)!=1){// first get number of processes
-		perror("Error reading number of processes");
-		return -1;
-	} 
-	
-	
-	data =(struct element*) malloc(num*sizeof(struct element)); // Rerserve memory for array of structure
-
-	if(data==NULL){
-		perror("Error allocating data structure");
-		free(data);
-		return -1;
-	}
-
-	for (int i =0; i<num; i++){ // Store the data of the file int the array
-		if(fscanf(fidin, "%d %d %d", &data[i].product_id, &data[i].op, &data[i].units) ==3);
-		else{
-			perror("Error reading the file");
+		if(cons<=0){
+			perror("Invalid number of consumers");
+			return -1;
+		}
+		if(bsize<=0){
+			perror("Invalid size of buffer");
+			return -1;
+		}
+		
+		// ***************** Loading data from input file into memory ****************
+		// Reading the input file
+		FILE *fidin = NULL;
+		
+		fidin = fopen(fileName,"r"); // Open the file
+		if (!fidin){
+			perror("Error opening the file");
 			return 1;
 		}
+		if(fscanf(fidin,"%d", &num)!=1){// first get number of processes
+			perror("Error reading number of processes");
+			return -1;
+		} 
+		
+		
+		data =(struct element*) malloc(num*sizeof(struct element)); // Rerserve memory for array of structure
+
+		if(data==NULL){
+			perror("Error allocating data structure");
+			free(data);
+			return -1;
+		}
+
+		for (int i =0; i<num; i++){ // Store the data of the file int the array
+			if(fscanf(fidin, "%d %d %d", &data[i].product_id, &data[i].op, &data[i].units) ==3);
+			else{
+				perror("Error reading the file");
+				return 1;
+			}
+		}
+		fclose(fidin);
+
+		//Creating the consumers and producers
+		pthread_t thr[prods];
+		pthread_t ths[cons];
+		for (i=0;i<prods;i++) {
+			pthread_create(&thr[i], NULL, producers, NULL);
+		}
+		for (i=0;i<cons;i++) {
+			pthread_create(&ths[i], NULL, consumers, NULL);
+		}
+
+		//Running the consumers and producers
+		for (i=0;i<prods;i++) {
+			pthread_join(thr[i],NULL);
+		}
+		for (i=0;i<cons;i++) {
+			pthread_join(ths[i],NULL);
+		}
+
+
+
+	// Output
+	printf("Total: %d euros\n", profits);
+	printf("Stock:\n");
+	printf("  Product 1: %d\n", product_stock[0]);
+	printf("  Product 2: %d\n", product_stock[1]);
+	printf("  Product 3: %d\n", product_stock[2]);
+	printf("  Product 4: %d\n", product_stock[3]);
+	printf("  Product 5: %d\n", product_stock[4]);
+
+	return 0;
 	}
-	fclose(fidin);
-
-
-
-  // Output
-  printf("Total: %d euros\n", profits);
-  printf("Stock:\n");
-  printf("  Product 1: %d\n", product_stock[0]);
-  printf("  Product 2: %d\n", product_stock[1]);
-  printf("  Product 3: %d\n", product_stock[2]);
-  printf("  Product 4: %d\n", product_stock[3]);
-  printf("  Product 5: %d\n", product_stock[4]);
-
-  return 0;
-}
