2d5f8d3d5637b533b8807e2074e55893c9b8be27
laaumartin <148004040+laaumartin@users.noreply.github.com>
2024-05-09 22:27:16 +0000

    ya no bucle inf, ahora segment fault

--- p3_multithread_2024/store_manager.c
+++ p3_multithread_2024/store_manager.c
@@ -36,13 +36,13 @@ pthread_mutex_t mutex;
 void *producers(void *args){
 	arguments *argsProducer = (arguments *)args; // Casting the void pointer to arguments pointer
 	for(int i=argsProducer->first; i < argsProducer->last ; i++ ) {
-		pthread_mutex_lock(&mutex); /* access to buffer*/
-		if (pthread_mutex_lock(&mutex) != 0) { 
+		
+		if (pthread_mutex_lock(&mutex) != 0) {  /* access to buffer*/
             perror("There has been an error executing the mutex lock");
             exit(1);
         }
 		while (queue_full(buffer)){ /* when buffer is full*/
-			pthread_cond_wait(&non_full, &mutex); 
+			
 			if (pthread_cond_wait(&non_full, &mutex) != 0) { 
                 perror("Waiting on the condition variable has failed");
                 exit(2);
@@ -51,12 +51,12 @@ void *producers(void *args){
 		if (queue_put(buffer, &data[i])<0){ // trying to save data[i] on the buffer
 			perror("there has been an error trying to save data on the buffer");
 		}
-		pthread_cond_signal(&non_empty); /* buffer is not empty */
-		if (pthread_cond_signal(&non_empty)!=0){ 
+		
+		if (pthread_cond_signal(&non_empty)!=0){ /* buffer is not empty */
 			perror("There has been an error when producing the signal for non empty");
 			exit(3);
 		}
-		pthread_mutex_unlock(&mutex);
+		
 		if (pthread_mutex_unlock(&mutex)!=0){ 
 			printf("There has been an error executing the mutex unlock\n");
 			exit(4);
@@ -68,13 +68,13 @@ void *producers(void *args){
 void *consumers(void *args) {
 	arguments *argsConsumer = (arguments *)args;
 	for(int i=0; i < argsConsumer->last ; i++ ){
-		pthread_mutex_lock(&mutex); // access to buffer
-		if (pthread_mutex_lock(&mutex) != 0) { 
+		
+		if (pthread_mutex_lock(&mutex) != 0) { // access to buffer
             perror("There has been an error executing the mutex lock");
             exit(1);
         }
 		while (queue_empty(buffer)){ //buffer is empty
-			pthread_cond_wait(&non_empty, &mutex); 
+			
 			if (pthread_cond_wait(&non_empty, &mutex) != 0) { 
                 perror("Waiting on the condition variable has failed");
                 exit(2);
@@ -154,13 +154,13 @@ void *consumers(void *args) {
 		else{ //If operation is neither PURCHASE nor SALE
 			perror("Invalid operation");
 		}
-		pthread_cond_signal(&non_full); //producing signal non_full
-		if (pthread_cond_signal(&non_full)!=0){ 
+		
+		if (pthread_cond_signal(&non_full)!=0){ //producing signal non_full
 			perror("There has been an error when producing the signal for non full");
 			exit(4);
 		}
-		pthread_mutex_unlock(&mutex); //unlocking mutex
-		if (pthread_mutex_unlock(&mutex)!=0){ 
+		
+		if (pthread_mutex_unlock(&mutex)!=0){ //unlocking mutex
 			printf("There has been an error executing the mutex unlock\n");
 			exit(5);
 		}
@@ -188,34 +188,34 @@ int main (int argc, const char * argv[])
 		}
 
 		const char *fileName = argv[1];
-		int prods = atoi(argv[2]);
-		int cons = atoi(argv[3]);
-		int bsize = atoi(argv[4]);
+		int producer_num = atoi(argv[2]);
+		int consumer_num = atoi(argv[3]);
+		int buffer_size = atoi(argv[4]);
 			
-		if(prods<=0){
+		if(producer_num<=0){
 			perror("Invalid number of producers");
 			return -1;
 		}
 		
-		if(cons<=0){
+		if(consumer_num<=0){
 			perror("Invalid number of consumers");
 			return -1;
 		}
-		if(bsize<=0){
+		if(buffer_size<=0){
 			perror("Invalid size of buffer");
 			return -1;
 		}
 		
-		// ***************** Loading data from input file into memory ****************
-		// Reading the input file
-		FILE *fidin = NULL;
 		
-		fidin = fopen(fileName,"r"); // Open the file
-		if (!fidin){
+		// Reading the input file
+		FILE *file = NULL;
+		
+		file = fopen(fileName,"r"); // Open the file
+		if (!file){
 			perror("Error opening the file");
 			return 1;
 		}
-		if(fscanf(fidin,"%d", &num)!=1){// first get number of processes
+		if(fscanf(file,"%d", &num)!=1){// first get number of processes
 			perror("Error reading number of processes");
 			return -1;
 		} 
@@ -233,7 +233,7 @@ int main (int argc, const char * argv[])
 			
 			char operation[20];
 
-			if(fscanf(fidin, "%d %s %d", &data[i].product_id, operation, &data[i].units) != 3) {
+			if(fscanf(file, "%d %s %d", &data[i].product_id, operation, &data[i].units) != 3) {
 				perror("Error reading the file");
 				return 1;
 			}
@@ -247,49 +247,49 @@ int main (int argc, const char * argv[])
 				return 1;
 			}
 		}
-		fclose(fidin);
+		fclose(file);
 
 		//Creating the circular buffer
-		queue* buffer = queue_init(bsize);
+		queue* buffer = queue_init(buffer_size);
 		if(buffer == NULL) { // Checking if queue initialization was successful
 			printf("Queue initialization failed\n");
 			return 1;
 		}
 
 		//Creating the consumers and producers
-		pthread_t thrProducer[prods];
-		pthread_t thrConsumer[cons];
+		pthread_t thrProducer[producer_num];
+		pthread_t thrConsumer[consumer_num];
 		arguments *argsProducer = malloc(num*sizeof(arguments));
 		arguments *argsConsumer = malloc(num*sizeof(arguments));
-		for (int i=0;i<prods;i++) {
+		for (int i=0;i<producer_num;i++) {
 			//here we will asign the range of data that will be processed for each producer
-			if (i != prods - 1) {
-				argsProducer[i].first = ceil(i * num / prods);
-				argsProducer[i].last = ceil((i + 1) * num / prods);
+			if (i != producer_num - 1) {
+				argsProducer[i].first = ceil(i * num / producer_num);
+				argsProducer[i].last = ceil((i + 1) * num / producer_num);
 			} else {
-				argsProducer[i].first = ceil(i * num / prods);
+				argsProducer[i].first = ceil(i * num / producer_num);
 				argsProducer[i].last = num;
 			}
 			pthread_create(&thrProducer[i], NULL, producers, &argsProducer[i]);
 		}
-		for (int i=0;i<cons;i++) {
-			if (i!= cons-1){
+		for (int i=0;i<consumer_num;i++) {
+			if (i!= consumer_num-1){
 				argsConsumer[i].first =0;
-				argsConsumer[i].last  = ceil(num/cons);
+				argsConsumer[i].last  = ceil(num/consumer_num);
 			} else{
 				argsConsumer[i].first = 0;
-				argsConsumer[i].last  = ceil(num/cons) + num % cons;
+				argsConsumer[i].last  = ceil(num/consumer_num) + num % consumer_num;
 			}
 			pthread_create(&thrConsumer[i], NULL, consumers, &argsConsumer[i]);
 		}
 
 		//Waiting the consumers and producers to finish execution
-		for (int i=0;i<prods;i++) {
+		for (int i=0;i<producer_num;i++) {
 			pthread_join(thrProducer[i],NULL);
 		}
 		producWorking=0;
 		pthread_cond_broadcast(&non_empty);
-		for (int i=0;i<cons;i++) {
+		for (int i=0;i<consumer_num;i++) {
 			pthread_join(thrConsumer[i],NULL);
 		}
 
@@ -309,7 +309,6 @@ int main (int argc, const char * argv[])
 		printf("  Product 4: %d\n", product_stock[3]);
 		printf("  Product 5: %d\n", product_stock[4]);
 
-		
 
 	return 0;
 	}
