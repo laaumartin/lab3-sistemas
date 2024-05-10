875cb3a679ff233d500f68e048b8704d181d0e23
luciacaldado <165171765+luciacaldado@users.noreply.github.com>
2024-05-10 17:09:07 +0000

    actualizacion10000

--- p3_multithread_2024/store_manager.c
+++ p3_multithread_2024/store_manager.c
@@ -17,34 +17,34 @@
 //Global variables needed on producers and consumers
 struct element *data; 
 queue *buffer;
-int num; // Number of operations (first line of the input file)
+int numOperations; // Number of operations (first line of the input file)
+int count=0;
+int producWorking=1;
 
 typedef struct arguments{
-	//fileInfo *arr;
 	int first, last;
 }arguments;
 
-struct result{
+typedef struct result{
 	int partial_profit;
 	int product_stock[5];
-};
+}result;
 
 //Initialize mutex and condition variables
-pthread_cond_t non_full ; /* can we add more elements? */
-pthread_cond_t non_empty ; /* can we remove elements? */
-pthread_mutex_t mutex ;
+pthread_cond_t non_full; /* can we add more elements? */
+pthread_cond_t non_empty; /* can we remove elements? */
+pthread_mutex_t mutex;
 
 
 void *producers(void *args){
 	arguments *argsProducer = (arguments *)args; // Casting the void pointer to arguments pointer
 	for(int i=argsProducer->first; i < argsProducer->last ; i++ ) {
-		
-		if (pthread_mutex_lock(&mutex) != 0) {  /* access to buffer*/
+		/* access to buffer*/
+		if (pthread_mutex_lock(&mutex) != 0) { 
             perror("There has been an error executing the mutex lock");
             exit(1);
         }
-		while (queue_full(buffer)){ /* when buffer is full*/
-			
+		while (queue_full(buffer)){ /* when buffer is full*/ 
 			if (pthread_cond_wait(&non_full, &mutex) != 0) { 
                 perror("Waiting on the condition variable has failed");
                 exit(2);
@@ -53,12 +53,11 @@ void *producers(void *args){
 		if (queue_put(buffer, &data[i])<0){ // trying to save data[i] on the buffer
 			perror("there has been an error trying to save data on the buffer");
 		}
-		
-		if (pthread_cond_signal(&non_empty)!=0){ /* buffer is not empty */
+		/* buffer is not empty */
+		if (pthread_cond_signal(&non_empty)!=0){ 
 			perror("There has been an error when producing the signal for non empty");
 			exit(3);
 		}
-		
 		if (pthread_mutex_unlock(&mutex)!=0){ 
 			printf("There has been an error executing the mutex unlock\n");
 			exit(4);
@@ -67,112 +66,123 @@ void *producers(void *args){
 	pthread_exit(0);
 }
 
-void *consumers(void *args) {
-	arguments *argsConsumer = (arguments *)args;
+void *consumers() {
+	struct element *Actelem;
 	struct result *result = malloc(sizeof(struct result));
 	result->partial_profit = 0;
 	memcpy(result->product_stock, (int[5]){0},sizeof(result->product_stock));
-	for(int i=0; i < argsConsumer->last ; i++ ){
-		
-		if (pthread_mutex_lock(&mutex) != 0) { // access to buffer
+	while(queue_empty(buffer)==0 || producWorking==1){
+		 // access to buffer
+		if (pthread_mutex_lock(&mutex) != 0) { 
             perror("There has been an error executing the mutex lock");
             exit(1);
         }
-		while (queue_empty(buffer)){ //buffer is empty
-			
+		while (queue_empty(buffer) && producWorking==1 ){ //buffer is empty
 			if (pthread_cond_wait(&non_empty, &mutex) != 0) { 
                 perror("Waiting on the condition variable has failed");
                 exit(2);
             }
 		}
 		//Now we compute profit and stock
-		int price;
-		struct element* Actelem ;
-		Actelem = queue_get(buffer);
-		if(Actelem==NULL){
-			perror("there has been an error trying to get data from the buffer");
-			exit(3);
-		}
-		//let's compute the purchase cost or the sale price of the element obtained from buffer
-		switch (Actelem->product_id)
-		{
-		case 1:
-			if(Actelem->op ==0){ //purchase cost for product 1
-				price=2;
+		if(queue_empty(buffer)==0){
+			int price;
+			Actelem = queue_get(buffer);
+			if(Actelem==NULL){
+				perror("there has been an error trying to get data from the buffer");
+				exit(3);
 			}
-			else if(Actelem->op ==1){ //sale price for product 1
-				price=3;
+			//let's compute the purchase cost or the sale price of the element obtained from buffer
+			switch (Actelem->product_id)
+			{
+			case 1:
+				if(Actelem->op ==0){ //purchase cost for product 1
+					price=2;
+				}
+				else if(Actelem->op ==1){ //sale price for product 1
+					price=3;
+				}
+				break;
+			case 2:
+				if(Actelem->op ==0){ //purchase cost for product 2
+					price=5;
+				}
+				else if(Actelem->op ==1){ //sale price for product 2
+					price=10;
+				}
+				break;
+			case 3:
+				if(Actelem->op ==0){ //purchase cost for product 3
+					price=15;
+				}
+				else if(Actelem->op ==1){ //sale price for product 3
+					price=20;
+				}
+				break;
+			case 4:
+				if(Actelem->op ==0){ //purchase cost for product 4
+					price=25;
+				}
+				else if(Actelem->op ==1){ //sale price for product 4
+					price=40;
+				}
+				break;
+			case 5:
+				if(Actelem->op ==0){ //purchase cost for product 5
+					price=100;
+				}
+				else if(Actelem->op ==1){ //sale price for product 5
+					price=125;
+				}
+				break;
+			default: //case where product_id is not valid
+				perror("Invalid product_id");
+				break;
 			}
+			//Now we compute the profits and product_stock after the operation
+			if(Actelem->op==0){ //Case where operation is PURCHASE
+				result->product_stock[Actelem->product_id - 1] += Actelem->units;
+				result->partial_profit -= price*Actelem->units;
+			}
+			else if(Actelem->op==1){ //Case where operation is SALE
+				if(result->product_stock[Actelem->product_id - 1] < Actelem->units){
+					//sale is not performed as there are NOT enough stock
+					printf("The product stock is less than the requested quantity.");
+				}
+				else{
+					//sale is performed as there are enough stock
+					result->product_stock[Actelem->product_id - 1] -= Actelem->units;
+					result->partial_profit += price*Actelem->units;
+				}
+			}
+			else{ //If operation is neither PURCHASE nor SALE
+				perror("Invalid operation");
+			}
+			free(Actelem);
+			count ++;
+			if (count>=numOperations){
+				producWorking=0;
+				pthread_cond_broadcast(&non_empty);
+				pthread_mutex_unlock(&mutex);
+				break;
+			}
+			//producing signal non_full
+			if (pthread_cond_signal(&non_full)!=0){ 
+				perror("There has been an error when producing the signal for non full");
+				exit(4);
+			}
+			//unlocking mutex
+			if (pthread_mutex_unlock(&mutex)!=0){ 
+				printf("There has been an error executing the mutex unlock\n");
+				exit(5);
+			}
+		} else{
+			pthread_mutex_unlock(&mutex);
 			break;
-		case 2:
-			if(Actelem->op ==0){ //purchase cost for product 2
-				price=5;
-			}
-			else if(Actelem->op ==1){ //sale price for product 2
-				price=10;
-			}
-			break;
-		case 3:
-			if(Actelem->op ==0){ //purchase cost for product 3
-				price=15;
-			}
-			else if(Actelem->op ==1){ //sale price for product 3
-				price=20;
-			}
-			break;
-		case 4:
-			if(Actelem->op ==0){ //purchase cost for product 4
-				price=25;
-			}
-			else if(Actelem->op ==1){ //sale price for product 4
-				price=40;
-			}
-			break;
-		case 5:
-			if(Actelem->op ==0){ //purchase cost for product 5
-				price=100;
-			}
-			else if(Actelem->op ==1){ //sale price for product 5
-				price=125;
-			}
-			break;
-		default: //case where product_id is not valid
-			perror("Invalid product_id");
-			break;
 		}
-		//Now we compute the profits and product_stock after the operation
-		if(Actelem->op==0){ //Case where operation is PURCHASE
-			result->product_stock[Actelem->product_id - 1] += Actelem->units;
-			result->partial_profit -= price*Actelem->units;
-		}
-		else if(Actelem->op==1){ //Case where operation is SALE
-			if(result->product_stock[Actelem->product_id - 1] < Actelem->units){
-				//sale is not performed as there are NOT enough stock
-				printf("The product stock is less than the requested quantity.");
-			}
-			else{
-				//sale is performed as there are enough stock
-				result->product_stock[Actelem->product_id - 1] -= Actelem->units;
-				result->partial_profit += price*Actelem->units;
-			}
-		}
-		else{ //If operation is neither PURCHASE nor SALE
-			perror("Invalid operation");
-		}
-		
-		if (pthread_cond_signal(&non_full)!=0){ //producing signal non_full
-			perror("There has been an error when producing the signal for non full");
-			exit(4);
-		}
-		
-		if (pthread_mutex_unlock(&mutex)!=0){ //unlocking mutex
-			printf("There has been an error executing the mutex unlock\n");
-			exit(5);
-		}
-		
 	}
 	pthread_exit(result);
 }
+
 
 
 int main (int argc, const char * argv[])
@@ -192,38 +202,40 @@ int main (int argc, const char * argv[])
 		}
 
 		const char *fileName = argv[1];
-		int producer_num = atoi(argv[2]);
-		int consumer_num = atoi(argv[3]);
-		int buffer_size = atoi(argv[4]);
+		int numP = atoi(argv[2]);
+		int numC = atoi(argv[3]);
+		int buffsize = atoi(argv[4]);
 			
-		if(producer_num<=0){
+		if(numP<=0){
 			perror("Invalid number of producers");
 			return -1;
 		}
 		
-		if(consumer_num<=0){
+		if(numC<=0){
 			perror("Invalid number of consumers");
 			return -1;
 		}
-		if(buffer_size<=0){
+		if(buffsize<=0){
 			perror("Invalid size of buffer");
 			return -1;
 		}
 		
+		// ***************** Loading data from input file into memory ****************
 		// Reading the input file
-		FILE *file = NULL;
+		FILE *fidin = NULL;
 		
-		file = fopen(fileName,"r"); // Open the file
-		if (!file){
+		fidin = fopen(fileName,"r"); // Open the file
+		if (!fidin){
 			perror("Error opening the file");
 			return 1;
 		}
-		if(fscanf(file,"%d", &num)!=1){// first get number of processes
-			perror("Error reading number of processes");
+		if(fscanf(fidin,"%d", &numOperations)!=1){// first get number of operations
+			perror("Error reading number of operations");
 			return -1;
 		} 
 		
-		data =(struct element*) malloc(num*sizeof(struct element)); // Rerserve memory for array of structure
+		
+		data =(struct element*) malloc(numOperations*sizeof(struct element)); // Rerserve memory for array of structure
 
 		if(data==NULL){
 			perror("Error allocating data structure");
@@ -231,11 +243,11 @@ int main (int argc, const char * argv[])
 			return -1;
 		}
 
-		for(int i=0;i<num;i++){
+		for(int i=0;i<numOperations;i++){
 			
 			char operation[20];
 
-			if(fscanf(file, "%d %s %d", &data[i].product_id, operation, &data[i].units) != 3) {
+			if(fscanf(fidin, "%d %s %d", &data[i].product_id, operation, &data[i].units) != 3) {
 				perror("Error reading the file");
 				return 1;
 			}
@@ -249,60 +261,51 @@ int main (int argc, const char * argv[])
 				return 1;
 			}
 		}
-		fclose(file);
+		fclose(fidin);
 
 		//Creating the circular buffer
-		queue* buffer = queue_init(buffer_size);
+		queue* buffer = queue_init(buffsize);
 		if(buffer == NULL) { // Checking if queue initialization was successful
 			printf("Queue initialization failed\n");
 			return 1;
 		}
 
 		//Creating the consumers and producers
-		pthread_t thrProducer[producer_num];
-		pthread_t thrConsumer[consumer_num];
-		arguments *argsProducer = malloc(num*sizeof(arguments));
-		arguments *argsConsumer = malloc(num*sizeof(arguments));
-		for (int i=0;i<producer_num;i++) {
+		pthread_t thrProducer[numP];
+		pthread_t thrConsumer[numC];
+		arguments *argsProducer = malloc(numOperations*sizeof(arguments));
+		for (int i=0;i<numP;i++) {
 			//here we will asign the range of data that will be processed for each producer
-			if (i != producer_num - 1) {
-				argsProducer[i].first = ceil(i * num / producer_num);
-				argsProducer[i].last = ceil((i + 1) * num / producer_num);
+			if (i != numP - 1) {
+				argsProducer[i].first = ceil(i * (numOperations-1) / numP);
+				argsProducer[i].last = ceil((i + 1) * (numOperations-1) / numP);
 			} else {
-				argsProducer[i].first = ceil(i * num / producer_num);
-				argsProducer[i].last = num;
+				argsProducer[i].first = ceil(i * (numOperations-1) / numP);
+				argsProducer[i].last = numOperations-1;
 			}
 			pthread_create(&thrProducer[i], NULL, producers, &argsProducer[i]);
 		}
-		for (int i=0;i<consumer_num;i++) {
-			if (i!= consumer_num-1){
-				argsConsumer[i].first =0;
-				argsConsumer[i].last  = ceil(num/consumer_num);
-			} else{
-				argsConsumer[i].first = 0;
-				argsConsumer[i].last  = ceil(num/consumer_num) + num % consumer_num;
-			}
-			pthread_create(&thrConsumer[i], NULL, consumers, &argsConsumer[i]);
+		for (int i=0;i<numC;i++) {
+			pthread_create(&thrConsumer[i], NULL, consumers, NULL);
 		}
 
 		//Waiting the consumers and producers to finish execution
-		for (int i=0;i<producer_num;i++) {
+		for (int i=0;i<numP;i++) {
 			pthread_join(thrProducer[i],NULL);
 		}
-		
-		struct result **result = (struct result **)malloc(consumer_num * sizeof(struct result));
-		for (int i=0;i<consumer_num;i++) {
+
+		struct result **result = (struct result **)malloc(numC * sizeof(struct result));
+		for (int i=0;i<numC;i++) {
 			pthread_join(thrConsumer[i],(void **)&result[i]);
 			profits += result[i]->partial_profit;
 			for(int j=0;j<5;j++){
 				product_stock[j] += result[i]->product_stock[j];
 			}
-			
 		}
-		
-		//Free the reserved memory
+		//Free reserved data
 		free(data);
 		free(argsProducer);
+		free(result);
 		pthread_mutex_destroy(&mutex);
 		pthread_cond_destroy(&non_full);
 		pthread_cond_destroy(&non_empty);
